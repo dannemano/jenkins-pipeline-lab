da8811cbda5f6b4676e40635e88f662605649802
Daniel Hedenstedt <daniel.hedenstedt@gmail.com>
2017-05-14 15:39:50 +0200

    initital

---
+++ src/main/java/se/dbot/jenkinspipelinelabs/JenkinsPipelineLabsApplication.java
@@ -0,0 +1,12 @@
+package se.dbot.jenkinspipelinelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineLabsApplication.class, args);
+	}
+}
---
+++ src/test/java/se/dbot/jenkinspipelinelabs/JenkinsPipelineLabsApplicationTests.java
@@ -0,0 +1,16 @@
+package se.dbot.jenkinspipelinelabs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsPipelineLabsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
